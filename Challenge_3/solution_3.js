e7eb652bc313d604fc2d2e5d9852988b9bf89637
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2023-06-18 23:57:16 +0300

    made changes to solution 3

--- Challenge_3/solution_3.js
+++ Challenge_3/solution_3.js
@@ -6,10 +6,10 @@
 let gross =parseFloat(prompt("Enter your gross salary"));
 
 let paye;
-if (gross <= 24,000){
+if (gross <= 24000){
     paye = 10
 }
-else if(gross > 24,000 && gross <= 32,333){
+else if(gross > 24000 && gross <= 32,333){
     paye = 25
 }else {
     paye = 30
@@ -17,44 +17,44 @@ else if(gross > 24,000 && gross <= 32,333){
 let tax = (gross * (paye/100))
 
 let nhif
-if (gross <= 5,999){
+if (gross <= 5999){
     nhif =150
-} else if(gross >= 6,000 && gross <= 7,999){
+} else if(gross >= 6000 && gross <= 7999){
     nhif = 300
-} else if (gross >= 8,000 && gross <= 11,999 ){
+} else if (gross >= 8000 && gross <= 11999 ){
     nhif = 400
-} else if (gross >= 12,000 && gross <= 14,999){
+} else if (gross >= 12000 && gross <= 14999){
     nhif = 500
-} else if (gross >= 15,000 && gross <= 19,999){
+} else if (gross >= 15000 && gross <= 19999){
     nhif = 600
-} else if (gross >= 20,000 && gross <= 24,999){
+} else if (gross >= 20000 && gross <= 24999){
     nhif =750
-} else if (gross >= 25,000 && gross <= 29,999){
+} else if (gross >= 25000 && gross <= 29999){
     nhif = 850
-} else if (gross >= 30,000 && gross <= 34,999){
+} else if (gross >= 30000 && gross <= 39999){
     nhif = 900
-} else if (gross >= 35,000 && gross <= 39,999){
+} else if (gross >= 35000 && gross <= 39999){
     nhif = 950
-} else if (gross >= 40,000 && gross <= 44,999){
-    nhif = 1,000
-} else if (gross >= 45,000 && gross <= 49,999){
+} else if (gross >= 40000 && gross <= 44999){
+    nhif = 1000
+} else if (gross >= 45000 && gross <= 49999){
     nhif = 1,100
-} else if (gross >= 50,000 && gross <= 59,999){
-    nhif = 1,200
-} else if (gross >= 60,000 && gross <= 69,999){
-    nhif = 1,300
-} else if ( gross >= 70,000 && gross <= 79,999){
-    nhif = 1,400
-} else if ( gross >= 80,000 && gross <= 89,999){
-    nhif = 1,500
-} else if (gross >= 90,000 && gross <= 99,999){
-    nhif = 1,600
-} else if (gross >= 100,000){
-    nhif = 1,700
+} else if (gross >= 50000 && gross <= 59999){
+    nhif = 1200
+} else if (gross >= 60000 && gross <= 69999){
+    nhif = 1300
+} else if ( gross >= 70000 && gross <= 79999){
+    nhif = 1400
+} else if ( gross >= 80000 && gross <= 89999){
+    nhif = 1500
+} else if (gross >= 90000 && gross <= 99999){
+    nhif = 1600
+} else if (gross >= 100000){
+    nhif = 1700
 }
 
 nssf = 400
 
 let netSalary = (gross - (tax + nhif + nssf));
-console.log( "Net salary is: " + netSalary )
+console.log( "Net salary is: " + netSalary);
 window.alert( "Your Net salary is:" + netSalary)
